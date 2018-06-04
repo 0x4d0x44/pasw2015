1fe29938c2f27db0e7a2ebdfe7426b0d8b7882f0
0x4d0x44 <0x4d0x44@users.noreply.github.com>
2018-06-04 17:42:56 +0200

    Update 404.php
    Aggiunta ricerca nel sito

--- 404.php
+++ 404.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
-    <h2 style="text-align: center;font-size: 20em;line-height: 0;margin-top: 0.5em;">404</h2>
-    <p style="text-align: center;font-size: 2em;color: #25385D;">Pagina non trovata</p>
+    <h2 style="text-align: center;font-size: 8em;line-height: 0;margin-top: 0.5em;">404</h2>
+    <p style="text-align: center;font-size: 1.5em;color: #25385D;">Siamo spieacenti, ma la pagina richiesta non esiste</p>
+    <p style="text-align: center;font-size: 1.5em;color: #25385D;">L'errore &egrave; stato gi&agrave; segnalato, nel frattempo &egrave; possibile effettuare una ricerca nel sito</p>
+    <?php get_search_form(); ?>
 </div>
 <?php get_sidebar('right'); ?>
 <?php get_footer(); ?>
