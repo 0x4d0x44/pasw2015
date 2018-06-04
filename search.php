58939332708859a114b70d668fa2f2112cb0f867
0x4d0x44 <0x4d0x44@users.noreply.github.com>
2018-06-04 16:30:30 +0200

    Update search.php
    Usa il formato configurato di default per la data

--- search.php
+++ search.php
@@ -8,7 +8,7 @@
         <?php while (have_posts()) : the_post(); ?>
             <div class="post">
                 <h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
-                <small><?php the_time('l, F jS, Y') ?></small>
+                <small><?php the_date() ?></small>
             </div>
         <?php endwhile; ?>
     <?php else : ?>
